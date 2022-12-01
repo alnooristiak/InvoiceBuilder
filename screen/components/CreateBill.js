37692ac007f9f9ccf39e84266aa31822cdf11755
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2022-12-01 19:30:51 +0600

    call bill section created

---
+++ screen/components/CreateBill.js
@@ -0,0 +1,38 @@
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+
+const CreateBill = () => {
+  return (
+    <View style={styles.billContainer}>
+      <Image style={styles.logo} source={require('../../assets/logo.png')} />
+      <TouchableOpacity style={styles.mkBill}>
+        <Text>Make a Bill</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+export default CreateBill
+
+const styles = StyleSheet.create({
+    billContainer: {
+        justifyContent: 'center',
+        alignContent: 'center',
+        flex: 1,
+    },
+    logo: {
+        width: 150,
+        height: 150,
+        justifyContent: 'center',
+        alignContent: 'center'
+    },
+    mkBill: {
+        width: 100,
+        height: 40,
+        backgroundColor: 'red',
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center',
+        borderRadius: 10,
+    }
+})
